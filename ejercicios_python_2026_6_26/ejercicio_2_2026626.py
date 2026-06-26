6b5df62608c301626b3793acb0ee891d746c0740
bruvePing <26751664+bruveping@users.noreply.github.com>
2026-06-26 18:48:00 -0300

    Update ejercicio_2_2026626.py

--- ejercicios_python_2026_6_26/ejercicio_2_2026626.py
+++ ejercicios_python_2026_6_26/ejercicio_2_2026626.py
@@ -1,8 +1,8 @@
 print('bien venido a la calculadora de propinas\n')
 elMonto=float(input("ingrese el monto de la comida\n"))
 elPorcentaje=float(input("ingrese el porcentaje de la propina\n"))
-cantidadComenzales=int(input("ingrese la cantidad de comenzales\n"))
-elresultado = ((elMonto * elPorcentaje) / 100) / cantidadComenzales
+cantidadComensales=int(input("ingrese la cantidad de comensales\n"))
+elresultado = ((elMonto * elPorcentaje) / 100) / cantidadComensales
 print("El monto de la propina que cada uno tiene que pagar es: \n")
 print(elresultado)
 
